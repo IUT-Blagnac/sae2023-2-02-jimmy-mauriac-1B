e2ea84d56d655cbd7720a7430e1916c7db5af0f6
jimmy <46294820+jimmy-txi@users.noreply.github.com>
2023-06-03 23:16:16 +0200

    adding algorithme

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -3,7 +3,23 @@ package exercice;
 import java.util.List;
 
 public class Exercice {
-    public static List<String> solution(String str, List<Character> ordre) {
-        throw new UnsupportedOperationException("Unimplemented");
+    public static ArrayList<String> solution(String str, ArrayList<String> ordre) {
+        String[] tableauMots = texte.split(" ");
+        ArrayList<String> mots = new ArrayList<>(Arrays.asList(tableauMots));
+        ArrayList<String> mots2 = new ArrayList<>(Arrays.asList(tableauMots));
+        for (int i= 0; i <ordre.size(); i++) {
+            System.out.println(ordre.get(i));
+            for( String mot : mots2) {
+                System.out.println(mot);
+                String lettre = String.valueOf(mot.charAt(0));
+                if( lettre.equals(ordre.get(i))) {
+                    System.out.println(i);
+                    mots.remove(mot);
+                    mots.add(i, mot);
+                }
+            }
+        }
+        
+        return mots;
     }
 }
--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         String texte = "exemple de texte a classer";
-        List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
+        ArrayList<String> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
         System.out.println(Exercice.solution(texte,ordre));
         // output: [classer, texte, exemple, a, de]
     }
